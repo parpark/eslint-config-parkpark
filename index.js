6e831920d17e797dffa44fdbfc7dfd0a91222be3
Andri Oskarsson <ao@parkpark.dk>
2018-08-13 12:19:55 +0200

    latest

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "globals": { "fetch": false, "alert": false, "__DEV__": true },
-    "env": {
-      "es6": true
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     }
-    "extends": [
-        "standard",
-        "prettier",
-        "prettier/react"
-    ],
-    "plugins": [
-        "prettier",
-        "react",
-        "standard"
-    ],
-    "rules": {
-        "prettier/prettier": "error",
-        "react/jsx-uses-react": "off"
-    }
-}
+  },
+  globals: { fetch: false, alert: false, __DEV__: true },
+  env: {
+    es6: true
+  },
+  extends: [
+    "standard",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react"
+  ],
+  plugins: ["prettier", "react", "standard"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error"
+  }
+};
